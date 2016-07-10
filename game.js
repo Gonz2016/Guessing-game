3811b70457042d75bad3aab9412c8865b0dd5081
Gonzalo <calderongonz@gmail.com>
2016-07-10 15:16:51 -0700

    incorporated functions to game

--- game.js
+++ game.js
@@ -9,27 +9,21 @@ if (answer.toLowerCase() == "yes") {
 var score = 0;
 var userName = prompt("What is your name?");
 alert("Nice to meet you " + userName + " lets play!");
-var brothers = prompt("Guess how many brothers do I have? between 1 and 5:","");
-if (brothers == 3) {
-  alert("Great Job");
-  score += 1;
-} else {
-  alert("Try again");
+
+function ask(question,correctAnswer) {
+  var answer = prompt(question,"");
+  if (answer == correctAnswer) {
+    alert("Great Job");
+    score += 1;
+  } else {
+    alert("Try again");
+  }
 }
-var sisters = prompt ("How many sisters do I have? between 1 and 7:","");
-if (sisters == 5) {
-  alert("Great job");
-  score += 1;
-} else {
-  alert("Try again");
-}
-var children = prompt("How many childrem do I have? between 1 and 10:","");
-if (children == 7) {
-  alert("Great job");
-  score += 1;
-} else {
-  alert("Try again");
-}
+
+ask("Guess how many brothers do I have? between 1 and 5:", 3);
+ask("Guess how many sisters do I have? between 1 and 7:", 5);
+ask("Guess how many children do I have? between 1 and 7:", 7);
+
 var cats = prompt("How many cats do I have? between 0 and 5:","")
 console.log(cats)
 if (cats == 3) {
